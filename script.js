9038c9ee4ffbf5fae2b3c2dbe898aa152d3c26ff
carbajal <210682@utags.edu.mx>
2023-01-25 14:48:55 -0600

    hola

---
+++ script.js
@@ -0,0 +1,10 @@
+function area() {
+        var ancho = document.getElementById("ancho").value;
+        var alto = document.getElementById("alto").value;
+        area = ancho  * alto;
+        return(area);
+    }
+
+
+var elemento = document.getElementById("pochef");
+elemento.innerHTML="<b> Pochef <b/>";
---
+++ sw.js
@@ -0,0 +1,30 @@
+const STATTIC_CACHE = "static";
+
+const APP_SHELL = [
+    "/",
+    "index.html",
+    "index.css",
+    "script.js",
+    "main.js",
+    "sw.js",
+    "21.jpg",
+];
+
+self.aaddEventListener("install", (e) =>{
+    const cacheStatic = caches
+        .open(STATTIC_CACHE)
+        .then((cache) => cache.addAll(APP_SHELL));
+    
+    e.waitUntill(cacheStatic);
+});
+
+self.addEventListener("fetch", (e) =>{
+    console.log("fetch! ", e.request);
+    
+    e.respondWith(
+        caches
+        .match(e.request)
+        .then(res => res || fetch(e.request))
+        .catch(console.log)
+    )
+});
